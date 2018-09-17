fdf866d224751961d74c6059166dea8ba52db0df
tarasmilchenko97 <panda.tara.18@gmail.com>
2018-09-17 23:22:13 +0300

    Add lesson_9

---
+++ lesson_9/index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+
+const surname = 'Мильченко ', name = 'Тарас';
+
+console.log(surname + name);
+
+ let a = 24, b = 35;
+
+if(a > b){
+    console.log(a);
+}else if(a < b) {
+    console.log(b);
+}else{
+    console.log("a=b");
+}
+
+if(a < b){
+    console.log(a);
+}else if(a > b) {
+    console.log(b);
+}else{
+    console.log("a=b");
+}
+
+if(a + b){
+    console.log(a + b);
+}
+
+if(a % b){
+    console.log(a % b);
+}else
+    console.log(0);
+
+
+if(a > (b + 10)){
+    console.log(a + 10);
+}else if(a < b){
+    console.log(9);
+}else{
+    console.log(b);
+}
+
+
+
